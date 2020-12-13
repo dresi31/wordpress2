671b7235a1809aed73531f15eeaf82fd4e0cef8f
dresi31 <mandressy@gmail.com>
2020-12-13 19:25:11 +0300

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'bqwiljmrcyso6ynwqcto' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'ufzc6ozwtb8yypkt' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'ufzc6ozwtb8yypkt' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'bqwiljmrcyso6ynwqcto-mysql.services.clever-cloud.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
